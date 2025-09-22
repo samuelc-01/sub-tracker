9695264b866905abe777aefbd5a108057992530a
samuelc-01 <samuelc.01dev@gmail.com>
2025-09-22 22:48:06 +0200

    create folder route with the auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,6 @@
 import { Router } from 'express';
-
+console.log("create auth")
 const authRoutes = Router();
-
 authRoutes.post('/sign-up', (req, res) => res.send({title: 'Sign Up'}));
 authRoutes.post('/sign-in', (req, res) => res.send({title: 'Sign in'}));
 authRoutes.post('/sign-out', (req, res) => res.send({title: 'Sign out'}));
