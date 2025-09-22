e67b94b4d52efae91811a956e959f35972c8276d
samuelc-01 <samuelc.01dev@gmail.com>
2025-09-22 22:48:44 +0200

    fix auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,3 +5,4 @@ authRoutes.post('/sign-up', (req, res) => res.send({title: 'Sign Up'}));
 authRoutes.post('/sign-in', (req, res) => res.send({title: 'Sign in'}));
 authRoutes.post('/sign-out', (req, res) => res.send({title: 'Sign out'}));
 
+export default authRoutes;
