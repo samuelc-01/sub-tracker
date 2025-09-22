d4e761b013f34b912cad7d63a829ccc97b0e16f4
samuelc-01 <samuelc.01dev@gmail.com>
2025-09-22 20:44:35 +0200

    create express server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("")
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("welcome to the seubscription tracker API")
+})
+
+app.listen(3000, () => {
+    console.log("Subscription Tracker API is running on http://localhost:3000")
+});
+
+export default app;
