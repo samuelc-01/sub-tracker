a5e3188de5f83915275d656db4a583bd696a3429
samuelc-01 <samuelc.01dev@gmail.com>
2025-09-23 23:13:45 +0200

    create routes auth, subscription, user

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-console.log("create auth")
-const authRoutes = Router();
-authRoutes.post('/sign-up', (req, res) => res.send({title: 'Sign Up'}));
-authRoutes.post('/sign-in', (req, res) => res.send({title: 'Sign in'}));
-authRoutes.post('/sign-out', (req, res) => res.send({title: 'Sign out'}));
 
-export default authRoutes;
+
+const authRouter = Router();
+authRouter.post('/sign-up', (req, res) => res.send({title: 'Sign Up'}));
+authRouter.post('/sign-in', (req, res) => res.send({title: 'Sign in'}));
+authRouter.post('/sign-out', (req, res) => res.send({title: 'Sign out'}));
+
+export default authRouter;
