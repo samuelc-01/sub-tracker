508cbe60552839b9263b35c6669b3bb1bf34c7f4
samuelc-01 <samuelc.01dev@gmail.com>
2025-09-23 23:34:51 +0200

    create db and env

--- config/env.js
+++ config/env.js
@@ -6,3 +6,4 @@ config({
 
 export const PORT = process.env.PORT || 3000;
 export const NODE_ENV = process.env.NODE_ENV || "development";
+export const DB_URI = process.env.DB_URI || "mongodb://localhost:27017";
