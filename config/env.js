b32c706a4f6c58441dc5412c12bd716f5683960b
samuelc-01 <samuelc.01dev@gmail.com>
2025-10-09 19:54:47 +0200

    setup jwt

--- config/env.js
+++ config/env.js
@@ -7,3 +7,5 @@ config({
 export const PORT = process.env.PORT || 3000;
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const DB_URI = process.env.DB_URI || "mongodb://localhost:27017";
+export const JWT_SECRET = process.env.JWT_SECRET || "";
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || 1000;
