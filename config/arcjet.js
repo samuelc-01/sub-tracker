9fa6d294fbd6ad0e2716af78e644bd1271aa4c29
samuelc-01 <samuelc.01dev@gmail.com>
2025-10-16 22:45:23 +0200

    feat: implement arcjet middleware and arcjet

---
+++ config/arcjet.js
@@ -0,0 +1,25 @@
+import { ARCJET_KEY } from './env.js'
+import arcjet, {shield, detectBot, tokenBucket} from '@arcjet/node';
+
+const aj = arcjet({
+
+  key: ARCJET_KEY,
+
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE"],
+
+    }),
+
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5,
+      interval: 10, 
+      capacity: 10, 
+    }),
+  ],
+});
+
+export default aj
