5aec8c4c1ca4b581d6bf069d141ca1fc7334f522
samuelc-01 <samuelc.01dev@gmail.com>
2025-10-22 16:29:52 +0200

    feat: add UpdateUser

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js"
+import bcrypt from 'bcryptjs';
+import mongoose from "mongoose"
 
 export const getUsers = async ( request, response, next ) => {
     try{
@@ -27,3 +29,34 @@ export const getUser = async ( request, response, next ) => {
     }
 }
 
+export const UpdateUser = async ( request, response, next) => {
+    try{
+        const { id } = request.params;
+        const updateData = request.body;
+
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            const error = new Error('Invalid user ID');
+            error.statusCode = 400;
+            throw error;
+        }
+        
+        const { password } = updateData;
+        if (password) { 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        updateData.password = hashedPassword;
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(id, updateData, { new: true }).select('-password');
+
+        if(!updatedUser){
+            const error = new Error('User not found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        response.status(200).json({ success: true, data: updatedUser });
+    }catch(e){
+        next(e)
+    }
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,13 @@
 import { Router } from 'express';
 import authorize from '../middlewares/auth.middleware.js'
-import { getUser, getUsers } from '../controllers/user.controller.js';
+import { getUser, getUsers, UpdateUser } from '../controllers/user.controller.js';
 import errorMiddleware from '../middlewares/error.middleware.js'
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
 userRouter.get('/:id', authorize , errorMiddleware, getUser );
-userRouter.post('/', (req, res) => res.send({title:"CREATE new user"}));
-userRouter.put('/:id', (req, res) => res.send({title:"UPDATE users"}));
+userRouter.put('/:id', authorize, errorMiddleware, UpdateUser);
 userRouter.delete('/:id', (req, res) => res.send({title:"DELETE user"}));
 
 export default userRouter;
