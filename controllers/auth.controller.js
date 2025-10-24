1b1b59d6d3f36a2c8a611124596b29c141d5b7fd
samuelc-01 <samuelc.01dev@gmail.com>
2025-10-24 16:33:19 +0200

    feat: add signOut function in auth.controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -80,4 +80,10 @@ export const signIn = async (req, res, next) => {
     }
 }
 
-export const signOut = async (req, res, next) => {}
+export const signOut = async (req, res, next) => {
+    try{
+        res.status(200).json({ success: true, message: 'User signed out successfully' })
+    } catch(error){
+        next(error)
+    }
+}
